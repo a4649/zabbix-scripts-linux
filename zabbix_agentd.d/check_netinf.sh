aeacf3ec0a9dd0a3e3fdb6752eff60ea63b0d6f6
Miguel Peixoto <63002031+a4649@users.noreply.github.com>
2021-09-23 18:51:07 +0100

    Update check_netinf.sh

--- zabbix_agentd.d/check_netinf.sh
+++ zabbix_agentd.d/check_netinf.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
-if ethtool $1 | grep yes > /dev/null
+INT=$1 #interface name
+OPT=$2 #operation
+
+if [ $OPT == "status" ]
 then
-    echo "1"
-else
-    echo "0"
+    STATUS="$(ethtool $INT | grep "Link detected:" | awk -F ' ' '{print $3}')"
+        if [ "$STATUS" == "yes" ]
+        then
+           echo "1" # Operational status is UP
+        else
+           echo "0" # Operational status is DOWN
+        fi
 fi
+
+# Transceiver Optical TX power in dBm
+if [ $OPT == "tx_power" ]
+then
+    ethtool -m $INT | grep -m1 "Laser output power" | awk -F ' ' '{print $8}'
+fi
+
+# Transceiver Optical RX power in dBm
+if [ $OPT == "rx_power" ]
+then
+    ethtool -m $INT | grep -m1 "Receiver signal average optical power" | awk -F ' ' '{print $10}'
+fi
+
+# Transceiver operational temperaature in ºC
+if [ $OPT == "temp" ]
+then
+    ethtool -m $INT | grep -m1 "Module temperature" | awk -F ' ' '{print $4}'
+fi
+
+# Ethernet speed of the link
+if [ $OPT == "speed" ]
+then
+    ethtool $INT | grep -m1 "Speed:" | awk -F ' ' '{print $2}'
+fi
+
+# Ethernet RX errors
+if [ $OPT == "rx_errors" ]
+then
+    ethtool -S $INT | grep HwIfInErrors | awk -F ' ' '{print $2}'
+fi
+
+# Ethernet TX errors
+if [ $OPT == "tx_errors" ]
+then
+    ethtool -S $INT | grep HwIfOutErrors | awk -F ' ' '{print $2}'
+fi
+
